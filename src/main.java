68e33abd129deb9dc89b5e94b45749acf8bccda4
the301sparton <chaitanya.deshp@gmail.com>
2018-08-25 22:12:15 +0530

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x,y,sum;
+        x = input.nextInt();
+        y = input.nextInt();
+        sum = x+y;
+        System.out.print("Sum = "+sum);
+    }
+}
